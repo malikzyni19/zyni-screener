b30b21ff555b7711bf81537ad705c392c3159bdc
malikzyni19 <malikzyni19@gmail.com>
2026-04-09 06:21:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2646,5 +2646,5 @@ def api_bias_scan():
 if __name__ == "__main__":
     import os
 
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port, debug=False)
